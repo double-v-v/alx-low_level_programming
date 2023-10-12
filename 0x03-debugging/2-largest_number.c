4cef673cd3ab6d3373d1c05a859bb9083321249f
Oladotun Tijani <oladotunwalod@gmail.com>
2023-10-12 12:32:13 -0700

    git

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,25 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	
 	int largest;
-	
-	if (a > b && a > c)
-	{
-		largest = a;
+
+	if (a >= b && a >= c)
+	{			
+		largest = a;	
+	}			
+	else if (b >= a && b >= c)	
+	{				
+		largest = b;	
+	}	
+	else			
+	{	
+		largest = c;	
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
-	{
-		largest = b;
-	}
-	else if (c > b)
-	{
-		largest = c;
-	}
-	
+
 	return (largest);
 }
