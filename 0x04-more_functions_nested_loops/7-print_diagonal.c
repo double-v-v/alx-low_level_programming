34cca9fe49b1b1e721ce7ea3d8096297ae72568a
Oladotun Tijani <oladotunwalod@gmail.com>
2023-10-12 23:02:21 -0700

    7 fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,9 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 0; i < n; i++)													{																for (j = 0; j < n; j++)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
 			{
 				if (j == i)
 					_putchar('\\');
